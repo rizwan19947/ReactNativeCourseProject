cebb8cdb2a2cb745c5f996752704070dd4d3212c
rizwan19947 <expectp@gmail.com>
2021-12-17 10:32:46 +0500

    Working with images and assets

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import ImageDetail from '../components/ImageDetail';
 
 
 const ImageScreen = () => {
 
 
-    return <Text>Image Screen</Text>;
+    return <View>
+
+        <ImageDetail title="Forest" imageSource={require("../../assets/forest.jpg")} imageScore="10" />
+        <ImageDetail title="Beach" imageSource={require("../../assets/beach.jpg")} imageScore="8"/>
+        <ImageDetail title="Mountain" imageSource={require("../../assets/mountain.jpg")} imageScore="9"/>
+
+
+    </View>
 
 };
 
